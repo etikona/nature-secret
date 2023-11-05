36a1980f5d7210e32ee87fd638652e145960c463
eti kona <etikonapal@gmail.com>
2023-11-05 22:27:07 +0600

    Navbar completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import close from "../assets/close.svg";
+import menu from "../assets/menu.svg";
+
+const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  //   const toggleNav = () => {
+  //     setIsNavOpen(!isNavOpen);
+  //   };
+  return (
+    <div className="navbar px-5 flex justify-between py-2 rounded w-full">
+      <div>
+        <Image src={logo} width={50} height={30} alt="Picture of the author" />
+      </div>
+      <nav className="">
+        <div className="md:hidden">
+          <Image
+            src={isNavOpen ? close : menu}
+            onClick={() => setIsNavOpen(!isNavOpen)}
+            className="text-white text-2xl"
+            aria-label="Toggle Menu"
+          ></Image>
+        </div>
+        <div
+          className={`${
+            isNavOpen ? "block" : "hidden"
+          } md:flex md:items-center md:w-auto`}
+        >
+          <ul className="flex  flex-col md:flex-row md:space-x-4 md:items-center space-x-4 text-white text-xl align-center items-center  ">
+            <li className="hover:text-green-500">
+              <Link href="/">Plant</Link>
+            </li>
+            <li className="hover:text-green-500">
+              <Link href="/profile">Profile</Link>
+            </li>
+            <li className="hover:text-green-500">
+              <Link href="/community">Community</Link>
+            </li>
+            <li className="hover:text-green-500">
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li className="hover:text-green-500">
+              <Link href="/contact">About</Link>
+            </li>
+            <li className="hover:text-green-500">
+              <Link href="/contact">Blog</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
